c861af0a27b1444c492a0fc4c30726ce56d9b33e
josh <19jsampsell@gmail.com>
2022-04-23 13:58:20 -0500

    fix build bot (maybe)

--- plugin/src/main/java/io/lumine/cosmetics/compat/LumineCoreCompat.java
+++ plugin/src/main/java/io/lumine/cosmetics/compat/LumineCoreCompat.java
@@ -2,12 +2,17 @@ package io.lumine.cosmetics.compat;
 
 import io.lumine.core.LumineCore;
 import io.lumine.core.players.PlayerProfile;
+import io.lumine.core.utils.gson.GsonProvider;
+import io.lumine.core.utils.logging.Log;
 import io.lumine.cosmetics.MCCosmeticsPlugin;
 import io.lumine.cosmetics.players.Profile;
 import io.lumine.utils.plugin.PluginModule;
 import io.lumine.utils.promise.Promise;
 import io.lumine.utils.storage.players.PlayerStorageAdapter;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -68,12 +73,44 @@ public class LumineCoreCompat {
             return promise;
         }
 
+
         @Override
         public Promise save(UUID uuid, Profile arg1) {
             return Promise.completed(true);
         }
 
-        private Optional<Profile> getFromCoreProfile(PlayerProfile coreProfile) {
+        @Override
+        public boolean saveSync(UUID key, Profile profile) {
+            File file = getFile(key.toString());
+
+            if(!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+                return false;
+            }
+
+        try(
+        FileWriter writer = new FileWriter(file)) {
+            GsonProvider.standard().toJson(profile, writer);
+            return true;
+        } catch(IOException e) {
+            Log.severe("Failed to save Json Profile in: " + this.getClass().getName());
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
+    protected File getFile(String key) {
+        return new File(plugin.getDataFolder() + "/data", key + ".json");
+    }
+
+
+    private Optional<Profile> getFromCoreProfile(PlayerProfile coreProfile) {
             var maybeProfile = coreProfile.getMetadata("MCCOSMETICS", Profile.class);
             
             if(maybeProfile.isPresent()) {
