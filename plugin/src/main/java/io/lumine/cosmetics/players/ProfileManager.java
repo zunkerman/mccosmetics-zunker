ea5c2aaf0c5ea3339a7fd6a743add9542e35d6c7
josh <19jsampsell@gmail.com>
2022-04-23 21:55:15 -0500

    Attempt to remove the cosmetics that get in the way when going into spectator mode.

--- plugin/src/main/java/io/lumine/cosmetics/players/ProfileManager.java
+++ plugin/src/main/java/io/lumine/cosmetics/players/ProfileManager.java
@@ -2,12 +2,16 @@ package io.lumine.cosmetics.players;
 
 import io.lumine.cosmetics.MCCosmeticsPlugin;
 import io.lumine.cosmetics.api.events.CosmeticPlayerLoadedEvent;
+import io.lumine.cosmetics.api.players.CosmeticProfile;
 import io.lumine.utils.Events;
 import io.lumine.utils.logging.Log;
 import io.lumine.utils.storage.players.PlayerRepository;
 import io.lumine.utils.storage.players.adapters.file.JsonPlayerStorageAdapter;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public class ProfileManager extends PlayerRepository<MCCosmeticsPlugin,Profile> {
@@ -22,7 +26,22 @@ public class ProfileManager extends PlayerRepository<MCCosmeticsPlugin,Profile> 
             default:
                 this.initialize(new JsonPlayerStorageAdapter<>(plugin,Profile.class));
                 break;
-        } 
+        }
+
+        Events.subscribe(PlayerGameModeChangeEvent.class).handler(event -> {
+           if(event.getNewGameMode().equals(GameMode.SPECTATOR)){
+               Optional<Profile> profile = this.getProfile(event.getPlayer().getUniqueId());
+
+               if(profile.isEmpty()){
+                   return;
+               }
+
+                profile.get().getEquipped().forEach(((aClass, equippedCosmetic) -> {
+                    equippedCosmetic.getCosmetic().getManager().unequip(profile.get());
+                }));
+
+           }
+        });
     }
 
     @Override
