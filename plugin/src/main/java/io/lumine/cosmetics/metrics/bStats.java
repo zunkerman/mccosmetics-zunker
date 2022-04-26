2880853fa78377cc3046ccafb73155745b3b458d
Ashijin <xikage@gmail.com>
2022-04-26 01:34:01 -0600

    Updated metrics

--- plugin/src/main/java/io/lumine/cosmetics/metrics/bStats.java
+++ plugin/src/main/java/io/lumine/cosmetics/metrics/bStats.java
@@ -3,18 +3,19 @@ package io.lumine.cosmetics.metrics;
 import io.lumine.cosmetics.MCCosmeticsPlugin;
 import io.lumine.cosmetics.logging.MCLogger;
 import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
 
 public class bStats {
 
 	public bStats(MCCosmeticsPlugin plugin)	{
 		try {
-        	Metrics metrics = new Metrics(plugin);
-
+        	Metrics metrics = new Metrics(plugin, 15050);
+        	
             final String devBuilds = plugin.getDescription().getVersion().contains("SNAPSHOT") ? "Yes" : "No";
             final String preBuilds = MCCosmeticsPlugin.isPremium() ? "Yes" : "No";
-
-            metrics.addCustomChart(new Metrics.SimplePie("premium", () -> preBuilds));
-            metrics.addCustomChart(new Metrics.SimplePie("devbuilds", () -> devBuilds));
+            
+            metrics.addCustomChart(new SimplePie("premium", () -> preBuilds));
+            metrics.addCustomChart(new SimplePie("devbuilds", () -> devBuilds));
             
             MCLogger.log("Started up bStats Metrics");
         } catch (Exception e) {
