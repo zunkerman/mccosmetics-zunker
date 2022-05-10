e5896e2105dd948245762805a2c830035a8cdac0
josh <19jsampsell@gmail.com>
2022-05-09 19:46:45 -0500

    Fix needing to have worldguard installed.

--- plugin/src/main/java/io/lumine/cosmetics/compat/WorldGuardSupport.java
+++ plugin/src/main/java/io/lumine/cosmetics/compat/WorldGuardSupport.java
@@ -1,11 +1,19 @@
 package io.lumine.cosmetics.compat;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.Flag;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
 import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
 import io.lumine.cosmetics.MCCosmeticsPlugin;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 public class WorldGuardSupport {
 
@@ -25,5 +33,15 @@ public class WorldGuardSupport {
             }
         }
 
+    }
+
+    public boolean canSprayWG(Player player, Location location) {
+        RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+        RegionQuery query = container.createQuery();
+        ApplicableRegionSet set = query.getApplicableRegions(BukkitAdapter.adapt(location));
+
+        LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
+
+        return set.testState(localPlayer, SPRAY_FLAG);
     }
 }
