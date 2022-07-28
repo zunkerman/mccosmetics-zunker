fbf8a4460944a7cae08b12386edbf25ac0507f30
Ashijin <xikage@gmail.com>
2022-07-27 22:02:52 -0600

    Fixed plugin not loading when using MythicMobs versions older than 5.0

--- plugin/src/main/java/io/lumine/cosmetics/compat/CompatibilityManager.java
+++ plugin/src/main/java/io/lumine/cosmetics/compat/CompatibilityManager.java
@@ -26,12 +26,22 @@ public class CompatibilityManager extends ReloadableModule<MCCosmeticsPlugin> {
             lumineCore = Optional.of(new LumineCoreCompat(plugin));
         }
         if(Bukkit.getPluginManager().getPlugin("MCPets") != null) {
-            Log.info("MCPets detected, enabling compatibility.");
-            mcpets = Optional.of(new MCPetsCompat(plugin));
+            try {
+                Log.info("MCPets detected, enabling compatibility.");
+                mcpets = Optional.of(new MCPetsCompat(plugin));
+            } catch(Exception | Error ex) {
+                Log.error("Failed to load MCPets compatibility, likely incompatible version");
+                mcpets = Optional.empty();
+            }
         }
         if(Bukkit.getPluginManager().getPlugin("MythicMobs") != null) {
             Log.info("MythicMobs found; enabling MythicMobs support.");
-            mythicMobs = Optional.of(new MythicMobsCompat(plugin));
+            try {
+                mythicMobs = Optional.of(new MythicMobsCompat(plugin));
+            } catch(Exception | Error ex) {
+                Log.error("Failed to load MythicMobs compatibility, likely incompatible version");
+                mythicMobs = Optional.empty();
+            }
         }
         if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
             Log.info("PlaceholderAPI found; enabling PlaceholderAPI support.");
